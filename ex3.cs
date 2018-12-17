547f2e249dc7f0278a42ee47f0a7a574a251641f
Moonyll <s.pouwel@laposte.net>
2018-12-17 16:12:32 +0100

    Ex3

---
+++ ex3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int div;
+            Console.WriteLine("a ?");
+           int a = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("b ?");
+            int b = int.Parse(Console.ReadLine());
+            a = 33 + a;
+            b = 1 + b; // ou b++;
+            div = a / b;
+            Console.WriteLine(div);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
